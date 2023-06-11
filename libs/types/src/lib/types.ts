c6085aaf5692cce84dba62cad1b87f21a9945bce
matar moveo <matarj@moveohls.com>
2023-06-11 18:24:23 +0300

    fix: removed unused imports types.ts

--- libs/types/src/lib/types.ts
+++ libs/types/src/lib/types.ts
@@ -1,5 +1,3 @@
-import { Request } from 'express';
-import { GetUserCommandOutput } from '@aws-sdk/client-cognito-identity-provider';
 export interface QueryRequest {
   search?: string;
   filter?: { [key: string]: [string] };
